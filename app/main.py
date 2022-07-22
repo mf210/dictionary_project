a83879838f6a6b4fc4f305d565d79acbd95ba82f
Mehdi Faresi <mfaresi210@gmail.com>
2022-07-22 10:28:16 +0430

    get phrase definition

--- app/main.py
+++ app/main.py
@@ -1,20 +1,40 @@
 import os
+from datetime import date
+from fastapi import FastAPI, Query
+from pymongo import MongoClient, ReturnDocument
 
-from fastapi import FastAPI
-from pymongo import MongoClient
+from .xfdictionary import get_data
 
-mongo_user = os.environ['MONGO_USER']
-mongo_pwd = os.environ['MONGO_PWD']
-
-mongo_client = MongoClient(f'mongodb://{mongo_user}:{mongo_pwd}@mongo', 27017)
 
 
 app = FastAPI()
 
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(
+        f"mongodb://{os.environ['MONGO_USER']}:{os.environ['MONGO_PWD']}@mongo", 27017)
+    app.dict_collection = app.mongodb_client.dictionarydb.dictionary
+    app.req_collection = app.mongodb_client.dictionarydb.requests
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
+
 @app.get('/')
-async def test_view():
-    dbs_name = mongo_client.list_database_names()
-    return {
-        'msg': 'Just a Test',
-        'dbs_list': dbs_name
-    }
+async def get_phrase_definitions(phrase: str = Query(max_length=60)):
+    # check if the phrase already exists in the database
+    result = app.dict_collection.find_one({'phrase': phrase})
+    if not result:
+        # update and check the today's request limit
+        today_req_count = app.req_collection.find_one_and_update(
+            { 'date': date.today().isoformat()},
+            { "$inc": { 'count': 1 }},
+            upsert=True,
+            return_document=ReturnDocument.AFTER)['count']
+
+        result = get_data(phrase) if today_req_count <= 10_000 else {'msg': "Today's capacity is completed"}
+        app.dict_collection.insert_one(result)
+
+    result.pop('_id', None)
+    result.pop('phrase', None)
+    return result
--- app/xfdictionary.py
+++ app/xfdictionary.py
@@ -1,15 +1,12 @@
-import requests
 import os
-
+import requests
 
 url = f"https://{os.environ['X_RAPIDAPI_HOST']}/v1/dictionary"
-
-querystring = {"selection":"successfully","textAfterSelection":"completed their project.","textBeforeSelection":"They"}
-
-payload = {
-	"selection": "successfully",
-	"textAfterSelection": "completed their project.",
-	"textBeforeSelection": "They"
+querystring = {
+	"antonyms":"true",
+	"audioFileLinks":"false",
+	"pronunciations":"false",
+	"relatedWords":"false",
 }
 headers = {
 	"content-type": "application/json",
@@ -17,6 +14,38 @@ headers = {
 	"X-RapidAPI-Host":  os.environ['X_RAPIDAPI_HOST']
 }
 
-response = requests.request("POST", url, json=payload, headers=headers, params=querystring)
+def get_data(phrase: str):
+	# create payload based on phrase
+	payload = {} 	
+	splited_phrase = phrase.split()
+	if len(splited_phrase) > 1:
+		payload['selection'] = splited_phrase[0]
+		payload['textAfterSelection'] = ' '.join(splited_phrase[1:])
+	else:
+		payload['selection'] = phrase
+	# get data from api
+	data = requests.request("POST", url, json=payload,
+							headers=headers, params=querystring).json()
+	# sort data
+	result = {
+		'phrase': phrase,
+		'definitions': [],
+		'examples': [],
+		'antonyms': [],
+		'synonyms': [],
+		'Other Forms': set()
+	}
+	for item in data.get('items', []):
+		result['antonyms'].extend(item.get('antonyms', []))
+		result['synonyms'].extend(item.get('synonyms', []))
+		for definition_obj in item.get('definitions', []):
+			result['definitions'].append(definition_obj['definition'])
+			result['examples'].extend(definition_obj.get('examples', []))
+		# extrac the other forms of phrase 
+		for inflectional_form in item.get('inflectionalForms', []):
+			other_form = inflectional_form['forms'][0]
+			if other_form:
+				result['Other Forms'].add(other_form)
 
-print(response.text)
+	result['Other Forms'] = list(result['Other Forms'])
+	return result
