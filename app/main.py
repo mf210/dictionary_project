f39ff893c4eeba224256013aeb30497b571ceecf
Mehdi Faresi <mfaresi210@gmail.com>
2022-07-23 10:58:10 +0430

    allow all origins

--- app/main.py
+++ app/main.py
@@ -10,17 +10,16 @@ from .xfdictionary import get_data
 
 app = FastAPI(docs_url="/translations/docs")
 
-origins = [
-    "https://www.youtube.com",
-    "http://localhost:8000", # for development purposes
-    "http://localhost:8022",
-    "http://127.0.0.1:8022", # to ensure
-
-]
+# origins = [
+#     "https://www.youtube.com",
+#     "http://localhost:8000", # for development purposes
+#     "http://localhost:8022",
+#     "http://127.0.0.1:8022", # to ensure
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
